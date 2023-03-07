4a1a61613b8ac3d428e7cb0991df7f7052eaffed
TimRumeev <trumeev@mail.com>
2023-03-07 22:55:26 +0500

    написаны controllers

---
+++ src/top-page/dto/find-top-page.dto.ts
@@ -0,0 +1,6 @@
+import { TopLevelCategory } from "../top-page.model/top-page.model";
+
+export class FindTopPageDto {
+	firstCategory: TopLevelCategory;
+
+}
--- src/top-page/top-page.model/top-page.model.ts
+++ src/top-page/top-page.model/top-page.model.ts
@@ -6,6 +6,7 @@ export enum TopLevelCategory {
 }
 
 export class TopPageModel {
+	_id: number;
 	firstLevelCategory: TopLevelCategory;
 	secondCategory: string;
 	title: string;
