254726a86fd0a5f5bb4b1d8e7e9eac0a6188fdcd
TimRumeev <trumeev@mail.com>
2023-03-10 21:18:33 +0500

    подключены модели и создан сервис отзывов

--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -1,9 +1,20 @@
 import { Module } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { TypegooseModule } from "nestjs-typegoose";
 import { TopPageController } from "./top-page.controller";
 
 @Module({
 	controllers: [TopPageController],
-	providers: [ConfigService]
+	providers: [ConfigService],
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: TopPageModule,
+				schemaOptions: {
+					collection: "TopPage	"
+				}
+			}
+		])
+	]
 })
 export class TopPageModule { }
